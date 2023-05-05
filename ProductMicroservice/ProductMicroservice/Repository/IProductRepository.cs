75659597bd362d14a1741ef85422ce6e77b0d424
PhilipNortje <111967136+PhilipNortje@users.noreply.github.com>
2023-05-05 12:59:07 +0200

    Project Files Added
    This project is about a Microservice

---
+++ ProductMicroservice/ProductMicroservice/Repository/IProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using ProductMicroservice.Model;
+using System.Collections.Generic;
+
+namespace ProductMicroservice.Repository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAll();
+
+        Product GetById(int id);
+
+        void InsertProduct(Product product);
+
+        void DeleteProduct(int ProductId);
+
+        void UpdateProduct(Product product);
+
+        void Save();
+        object GetProducts();
+        object GetProductByID(int id);
+    }
+}
